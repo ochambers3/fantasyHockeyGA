b147a7271c003e88957d402632d06ef82813965f
Owen Chambers <owenchambers@Owens-Laptop.local>
2023-11-13 16:01:23 -0600

    Finished Draft Method

--- Manager.py
+++ Manager.py
@@ -1,16 +1,34 @@
 class Manager:
-	def __init__(self, name, weights):
-		self.name = name
-		self.weights = weights
+    def __init__(self, name, weights):
+        self.name = name
+        self.weights = weights
+        self.positions = {
+                "F": 0,
+                "D": 0
+                }
+        self.roster = {} 
 
-	def getWeight(self, pos):
-		return self.weights[pos]
+    def getWeight(self, pos):
+        return self.weights[pos]
 
-	def updateWeight(self, pos, newVal):
-		self.weights[pos] = newVal
+    def updateWeight(self, pos, newVal):
+        self.weights[pos] = newVal
 
-	def displayManager(self):
-		print(self.name)
-		for i in range(0, len(self.weights)):
-			print("Weight ", i, ": ", self.getWeight(i))
-		return 10
+    def displayManager(self):
+        print(self.name)
+        print(self.weights[0], self.weights[1], self.weights[2], self.weights[3], self.weights[4], self.weights[5], self.weights[6])
+        print(self.roster)
+        return None
+
+    def getF(self):
+       return self.roster["F"]
+
+    def getD(self):
+       return self.roster["D"]
+
+
+    def setF(self):
+       self.roster["F"] += 1
+
+    def setD(self):
+       self.roster["D"] += 1
--- main.py
+++ main.py
@@ -1,11 +1,44 @@
 from Manager import *
+import pandas as pd
 
-startWeights = [0] * 6
+def draftPlayers(manager, playerStats, playerDict):
+    data = playerStats.copy()
+    operations = {
+            'G': lambda x: x * manager.weights[0],
+            'A': lambda x: x * manager.weights[1],
+            'SHG': lambda x: x * manager.weights[2],           
+            'GWG': lambda x: x * manager.weights[3],
+            'PPP': lambda x: x * manager.weights[4],
+            'SHOTS': lambda x: x * manager.weights[5],
+            'HITS': lambda x: x * manager.weights[6],
+            'BS': lambda x: x * manager.weights[7]}
+    columns_to_operate_on = ['G', 'A', 'SHG', 'GWG', 'PPP',  'SHOTS', 'HITS', 'BS']
 
-alec = Manager("alec", startWeights)
+    for column in columns_to_operate_on:
+        if column in operations:
+            operation = operations[column]
+            data[column] = operation(data[column])
+    print(data.head())
 
-print(alec.getWeight(2))
-alec.updateWeight(2, 5)
-print(alec.getWeight(2))
-print(alec.displayManager())
+    data['fitness'] = data[columns_to_operate_on].sum(axis=1)
+    player = data['fitness'].idxmax()
+    print("Player selected: ", data.iloc[player, 0], " ranked: ", player+1)
+    
 
+playerDict = {}
+
+#B-Name, E-Pos, G-Goals, H-Assists, S-Game Winning Goals, AA-Power Play Points, AR-Shots, AT-Hits, AU-Blocks
+playerStats = pd.read_excel('playerStats.xlsx', sheet_name=1, usecols="B,E,G,H,R,S,AA,AR,AT,AU", skiprows=1)
+print(playerStats.head())
+
+
+alec = Manager("alec", [0, 1, 2, 3, 4, 5, 6,7])
+owen =  Manager("owen", [6, 4, 0.25, 2, 0.9, 1, 0.5,7])
+charlie = Manager("charlie", [6, 4, 0.25, 2, 0.9, 1, 0.5,7])
+dave = Manager("Dave", [6, 4, 0.25, 2, 0.9, 1, 0.5,7])
+evan = Manager("Evan", [6, 4, 0.25, 2, 0.9, 1, 0.5,7])
+fred = Manager("Fred", [4, 5, 0.1, 0.5, 2, 3, 0.2,7])
+greg = Manager("Greg", [2, 8, 0.9, 4, 1, 2, 6,7])
+
+draftPlayers(owen, playerStats, playerDict)
+print(playerStats.head())
